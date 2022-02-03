4fc686be28806fa11132e4291081a677805ebb84
Vijay Chathe <76934330+vijay1213@users.noreply.github.com>
2022-02-03 19:57:42 +0530

    Add files via upload

---
+++ disturbed_people.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+#endif
+	
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	
+	int ans = 0;
+	for (int i = 1; i < n - 1; ++i) {
+		if (a[i] == 0 && a[i - 1] == 1 && a[i + 1] == 1) {
+			++ans;
+			a[i + 1] = 0;
+		}
+	}
+	cout << ans << endl;
+	
+	return 0;
+}
